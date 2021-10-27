1e89eef1818062cb28295a76981f903721dd775c
jrfreyes <justinrupert.reyes@gmail.com>
2021-10-27 21:39:24 +0900

    Update database.js

--- components/database.js
+++ components/database.js
@@ -1,28 +1,12 @@
 import { MongoClient } from "mongodb";
+import { readFileSync } from 'fs';
 
 let uri = 'mongodb://localhost:27017/';
 
 let client = new MongoClient(uri);
 
-const adobo = {
-    "title": "Adobo",
-    "ingredients": [
-        {"amt": "2", "unit": "lbs", "name": "pork belly"},
-        {"amt": "2", "unit": "tbsp", "name": "garlic minced"},
-        {"amt": "5", "unit": "pcs", "name": "dried bay leaves"},
-        {"amt": "4", "unit": "tbps", "name": "vinegar"},
-        {"amt": "1", "unit": "cup", "name": "soy sauce"},
-        {"amt": "1", "unit": "tablespoon", "name": "peppercorn"},
-        {"amt": "2", "unit": "cups", "name": "water"},
-        {"amt": "", "unit": "", "name": "Salt"}
-    ],
-    "steps": [
-        {"index": "1", "img": "placeholder.jpg", "text": "Do this"},
-        {"index": "2", "img": "placeholder.jpg", "text": "Do this next"},
-        {"index": "3", "img": "placeholder.jpg", "text": "Step 3"},
-        {"index": "4", "img": "placeholder.jpg", "text": "Serve"}
-    ]
-};
+let rawdata = readFileSync('../sample_schemas/recipe.json');
+let adobo = JSON.parse(rawdata);
 
 await client.connect();
 
